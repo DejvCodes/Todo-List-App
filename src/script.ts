f4ad4883d722f333fcad79adb87739c26e2951a1
David Kalmus <dejvios@seznam.cz>
2025-03-05 20:23:01 +0100

    script.ts

---
+++ src/script.ts
@@ -0,0 +1,57 @@
+// Variables
+const inputBox = document.querySelector('.input-form input') as HTMLInputElement
+const addBtn = document.querySelector('.input-form button') as HTMLButtonElement
+const todoItems = document.querySelector('.todo-items') as HTMLElement
+
+// Default values
+let keyword: string = ""
+
+// Task type
+type Task = { id: number, text: string}
+
+// Function addTask()
+const addTask = (): void => {
+    keyword = inputBox.value.trim()
+
+    if (keyword === "") {
+        alert("Please enter a new todo...")
+    } else {
+        const newTask: Task = {
+            id: new Date().getTime(),
+            text: keyword,
+        }
+        createNewTask(newTask)
+    }
+    inputBox.value = ""  
+}
+
+// Function createNewTask()
+const createNewTask = (task: Task): void => {
+    // Element li (class one-item)
+    const elementLi = document.createElement('li')
+    elementLi.setAttribute("data-id", task.id.toString())
+    elementLi.classList.add("one-item")
+    todoItems.appendChild(elementLi)
+
+    // Element p (class text)
+    const elementP = document.createElement('p')
+    elementP.innerHTML = task.text
+    elementP.classList.add("text")
+    elementLi.appendChild(elementP)
+
+    // Element span (class cross) - delete button
+    const elementSpan = document.createElement('span')
+    elementSpan.innerHTML = '\u00d7'
+    elementSpan.classList.add("cross")
+    elementLi.appendChild(elementSpan)
+}
+
+// Calling the addTask() function after the addBtn button is pressed
+if (addBtn) {
+    addBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        addTask()
+    })
+} else {
+    console.error("Add button not found!")
+}
